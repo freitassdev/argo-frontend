60c078e8523f1869f1132ba63d1ac3c64fd7ca80
zuifiti <chelmizinho@gmail.com>
2024-11-02 21:38:37 -0300

    feat: add base of product-item

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -6,15 +6,15 @@ export default function Home() {
   return (
     <div className="">
       <Navbar />
-      <div className="mt-24">
+      <div className="mt-24 flex flex-col gap-4">
         <div className="flex flex-col">
           <p className="text-md text-foreground">Seja bem vindo(a)!</p>
           <p className="text-lg text-foreground">Veja as principais promoções da semana, estão imperdíveis!</p>
-          <div className="grid grid-cols-4 gap-3">
-            {Array.from({ length: 20 }).map((_, index) => (
-              <Item key={index} />
-            ))}
-          </div>
+        </div>
+        <div className="grid grid-cols-4 gap-3">
+          {Array.from({ length: 20 }).map((_, index) => (
+            <Item key={index} />
+          ))}
         </div>
       </div>
     </div>
