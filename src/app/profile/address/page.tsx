dee688c9a014b647e703eaab1d5c6ab627e80878
freitassdev <chelmizinho@gmail.com>
2024-11-07 14:48:16 -0300

    feat: refactor address page

--- src/app/profile/address/page.tsx
+++ src/app/profile/address/page.tsx
@@ -1,28 +1,32 @@
 "use client"
+import GradientTitle from '@/components/shared/gradient-title/gradient-title';
 import Navbar from '@/components/shared/navbar/navbar';
+import ReturnButton from '@/components/shared/return-button/return-button';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Separator } from '@/components/ui/separator';
 
 export default function Address() {
     return (
-        <div className="flex flex-col items-center py-8">
+        <div className="flex flex-col w-full pb-8 gap-4 mt-24">
             <Navbar />
-            <h2 className="text-2xl font-semibold py-20">Meus Endereços</h2>
-            <hr className="border-t border-gray-400 mb-4 w-full max-w-2xl" />
-            <div className="w-full max-w-2xl space-y-4">
-                <div style={{backgroundColor: "#1a1a4a"}} className="text-white p-4 rounded">
+            <div className="flex flex-col gap-2">
+                <ReturnButton />
+                <GradientTitle>Edite seu endereço</GradientTitle>
+            </div>
+            <Separator />
+            <div className="w-full grid grid-cols-2 gap-3">
+                <div className="flex flex-col bg-primary text-white dark:text-foreground p-4 gap-3 rounded-xl">
                     <h3 className="text-lg font-semibold">Endereço 1</h3>
-                    <p style={{backgroundColor: "#37406e"}} className="p-2 rounded text-white mt-2">
-                        Rua, Bairro, Cidade, Estado, CEP
-                    </p>
+                    <Input disabled className="bg-white disabled:opacity-100     text-black" placeholder='Rua, Bairro, Cidade, Estado, CEP' />
                 </div>
-                <div style={{backgroundColor: "#1a1a4a"}} className="text-white p-4 rounded">
+                <div className="flex flex-col bg-primary text-white dark:text-foreground p-4 gap-3 rounded-xl">
                     <h3 className="text-lg font-semibold">Endereço 2</h3>
-                    <p style={{backgroundColor: "#37406e"}} className="p-2 rounded text-white mt-2">
-                        Rua, Bairro, Cidade, Estado, CEP
-                    </p>
+                    <Input disabled className="bg-white disabled:opacity-100     text-black" placeholder='Rua, Bairro, Cidade, Estado, CEP' />
                 </div>
-                <div className="flex flex-row space-x-4 mt-4">
-                    <button style={{backgroundColor: "#0c0726"}} className="text-white px-4 py-2 rounded-lg">Editar</button>
-                    <button style={{backgroundColor: "#0c0726"}} className="text-white px-4 py-2 rounded-lg">Adicionar</button>
+                <div className="flex flex-row gap-2 mt-4">
+                    <Button size={"lg"}>Editar</Button>
+                    <Button size={"lg"}>Adicionar</Button>
                 </div>
             </div>
         </div>
